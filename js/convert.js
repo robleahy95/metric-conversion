f9b695586b3fce37ae9a6342cf79dadebf436f44
robleahy95 <35769579+robleahy95@users.noreply.github.com>
2018-04-26 15:46:36 +0100

    Add files via upload
    Java script code fixed

--- js/convert.js
+++ js/convert.js
@@ -1,10 +1,13 @@
-function if {
-
+function feetToMeters (){
   var measure = parseInt(document.getElementById("value1").value);
 
-  var meters = measure x 0.3048;
+  var meters = measure * 0.3048;
   var message = measure + ' feet converts to ' + meters + ' meters.';
-  console.flag(message);
-  document.getElementbyId("resultsentence").innerHTML = message;
-
+  console.log(message)
 }
+
+
+
+
+
+
