f5575dd2b72386455ab394262e47db3c3d4b6609
robleahy95 <35769579+robleahy95@users.noreply.github.com>
2018-04-26 15:59:34 +0100

    Add files via upload
    code added to js file for new measurements in table 1

--- js/convert.js
+++ js/convert.js
@@ -7,6 +7,32 @@ function feetToMeters (){
   console.log(message)
 }
 
+function inchesToCentimeters (){
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var inches = measure * 2.54;
+  var message = measure + ' inches converts to ' + centimeters + ' centimeters.';
+  console.log(message)
+}
+
+function yardsToMeters (){
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var yards = measure * 0.9144;
+  var message = measure + ' yards converts to ' + meters + ' meters.';
+  console.log(message)
+}
+
+function milesToKilometers (){
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 1.60934;
+  var message = measure + ' miles converts to ' + kilometers + ' meters.';
+  console.log(message)
+}
+
+
+
 
 
 
