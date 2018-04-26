2cbfe998f16fbe3d054c31f77bf00c2d517a2063
robleahy95 <35769579+robleahy95@users.noreply.github.com>
2018-04-26 16:48:51 +0100

    Add files via upload
    Table 100% working. Variables corrected

--- js/convert.js
+++ js/convert.js
@@ -1,14 +1,18 @@
+//ensures user inputs value//
 var myAlert = "please enter correct measurement"
 var Null = "please enter measurement"
 
 function feetToMeters ()
 {
   var measure = parseInt(document.getElementById("value1").value);
-
+//conversion ratio//
   var meters = measure * 0.3048;
   var message = measure + ' feet converts to ' + meters + ' meters.';
   console.log(message)
-		
+  
+ 
+  
+		//ensures value is above 0//
 		if(!measure){
 			alert ("please enter value")
 		}
@@ -24,11 +28,9 @@ function inchesToCentimeters ()
   var measure = parseInt(document.getElementById("value1").value);
 
   var inches = measure * 2.54;
-  var message = measure + ' inches converts to ' + centimeters + ' centimeters.';
+  var message = measure + ' inches converts to ' + inches + ' centimeters.';
   console.log(message)
   
-document.getElementbyId("resultSentence").innerHTML = message;
-		
 		if(!measure){
 			alert ("please enter value")
 		}
@@ -45,10 +47,8 @@ function yardsToMeters ()
   var measure = parseInt(document.getElementById("value1").value);
 
   var yards = measure * 0.9144;
-  var message = measure + ' yards converts to ' + meters + ' meters.';
+  var message = measure + ' yards converts to ' + yards + ' meters.';
   console.log(message)
-  
-document.getElementbyId("resultSentence").innerHTML = message;
 	
 	if(!measure){
 			alert ("please enter value")
@@ -65,10 +65,8 @@ function milesToKilometers ()
   var measure = parseInt(document.getElementById("value1").value);
 
   var meters = measure * 1.60934;
-  var message = measure + ' miles converts to ' + kilometers + ' meters.';
+  var message = measure + ' miles converts to ' + meters + ' meters.';
   console.log(message)
-
-document.getElementbyId("resultSentence").innerHTML = message;
 	
 	if(!measure){
 			alert ("please enter value")
